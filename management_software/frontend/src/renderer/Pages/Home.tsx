bda07eba4271d889ec8d26abcbb431b3b7ee4e7c
Rico Chao <ricochao@d142-058-160-147.wireless.sfu.ca>
2022-04-06 16:24:56 -0700

    Tested request communication and fixed cor issue

--- management_software/frontend/src/renderer/Pages/Home.tsx
+++ management_software/frontend/src/renderer/Pages/Home.tsx
@@ -1,47 +1,60 @@
 import React from 'react';
+import axios from 'axios';
 import '../css/Home.css';
 
 //Layout tabs will soon be replaced with dynamic version
 export default function Home() {
-    return (
-      <div className="right-content">
-        <ul id="layout-tabs">
-          <li>
-            Main Dining
-          </li>
-          <li>
-            Outdoor
-          </li>
-        </ul>
-        <div id="layout">
-          <div id="layout-legend-content">
-            <div id="layout-legend">
-              <ul id="layout-legend-elements">
-                <li>
-                  <span className="legend-colors">
-                  </span>
-                  Free
-                </li>
-                <li>
-                  <span className="legend-colors">
-                  </span>
-                  Occupied
-                </li>
-                <li>
-                  <span className="legend-colors">
-                  </span>
-                  Need Cleaning
-                </li>
-                <li>
-                  <span className="legend-colors">
-                  </span>
-                  Attention
-                </li>
-              </ul>
-            </div>
+  const url = "http://127.0.0.1:5000/message";
+
+  function testFn() {
+    axios.get(url)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+  }
+
+  return (
+    <div className="right-content">
+      <ul id="layout-tabs">
+        <li>
+          Main Dining
+        </li>
+        <li>
+          Outdoor
+        </li>
+      </ul>
+      <div id="layout">
+        <div id="layout-legend-content">
+          <button onClick={testFn}>
+            WATER REFILL TEST
+          </button>
+
+          <div id="layout-legend">
+            <ul id="layout-legend-elements">
+              <li>
+                <span className="legend-colors">
+                </span>
+                Free
+              </li>
+              <li>
+                <span className="legend-colors">
+                </span>
+                Occupied
+              </li>
+              <li>
+                <span className="legend-colors">
+                </span>
+                Need Cleaning
+              </li>
+              <li>
+                <span className="legend-colors">
+                </span>
+                Attention
+              </li>
+            </ul>
           </div>
-          <div id="layout-content"></div>
         </div>
+        <div id="layout-content"></div>
       </div>
-    );
-  };
+    </div>
+  );
+};
