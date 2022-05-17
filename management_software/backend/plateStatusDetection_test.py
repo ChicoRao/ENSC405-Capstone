b1236b651a74e7d50c088363976235f00bec7c84
Patrick <congchenri.p@gmail.com>
2022-05-16 23:43:02 -0700

    modifed plateStatusDetection_test.py, resolved all the errors

--- management_software/backend/plateStatusDetection_test.py
+++ management_software/backend/plateStatusDetection_test.py
@@ -5,7 +5,6 @@ import cvlib as cv
 import numpy as np
 import sys
 from cvlib.object_detection import draw_bbox
-
 import torch
 from PIL import Image
 
@@ -18,13 +17,41 @@ def plateStatus():
 
     print ("111111111111111111111111")
     result = model(img)
+    # cv2.imshow('detection',result)
     print ("222222222222222222222222")
-    crops = result.crop(save=True)
-    print ("333333333333333333333333")
-    
-    print ("4444444444444444444444444")
+    crops = result.crop(save = False)
 
-    cv2.imshow('detection',img)
+    # x = crops.values()
+    # print (x)
+    print ("333333333333333333333333")
+    # print (len(crops))
+
+    for x in range(len(crops)):
+        conf = 0
+        count = 0
+        # print ((crops[x-1]['conf']))
+        if crops[x]['conf'] > conf:
+            conf = crops[x]['conf']
+            count = x
+    print(conf)
+    print (count)
+
+    crops = crops[count]
+    print (crops)
+
+
+    # for x in range (len(crops)):
+
+    #     if crops[x]['conf'] != conf:
+    #         crops.pop(x)
+    # print (crops) 
+
+
+    # print(crops[0]['im'])
+    # cv2.imshow('detection',crops[2]['im'])
+    # cv2.waitKey(0)
+    print ("44444444444444444444444")
+    # cv2.imshow('detection',crops)
 
 
 
@@ -34,10 +61,10 @@ def plateStatus():
     # For testing
     #cv2.imshow("crop_img", crop_img)
 
-    if not crops.all():
-        gray = cv2.cvtColor(crops, cv2.COLOR_BGR2GRAY)
+    if crops:
+        gray = cv2.cvtColor(crops['im'], cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray,(7,7),0)
-        ret, thresh = cv2.threshold(blurred, 127, 255, 0)
+        ret, thresh = cv2.threshold(gray, 127, 255, 0)
         #cv2.imshow("edges", edges)
         contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         #print(len(hierarchy[0]))
