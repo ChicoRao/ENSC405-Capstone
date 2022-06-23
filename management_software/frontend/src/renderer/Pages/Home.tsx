9f54ccb193eb4d9f9a0d83af9ec2ada60747d8df
Irene <ireneleung24@gmail.com>
2022-06-22 21:24:00 -0700

    display icon on Home page

--- management_software/frontend/src/renderer/Pages/Home.tsx
+++ management_software/frontend/src/renderer/Pages/Home.tsx
@@ -3,6 +3,48 @@ import axios from 'axios';
 import Tabs from '../Component/Tabs';
 import Layout from '../Component/Layout';
 import '../css/Home.css';
+import tableLogo from '../../../assets/icons/editor/table.svg';
+import chairLogo from '../../../assets/icons/editor/chair.svg';
+import cashierLogo from '../../../assets/icons/editor/cashier.png';
+import tableLogo1 from '../../../assets/icons/editor/Table1.svg';
+import boothTableLogo from '../../../assets/icons/editor/booth_table.png';
+import dividerLogo from '../../../assets/icons/editor/divider.png';
+import doorLogo from '../../../assets/icons/editor/door.png';
+
+interface LayoutDataCell {
+  id: string,
+  type: string,
+  icon: string,
+  top: string,
+  left: string
+}
+
+const symbolsList = [
+  {
+    name: "Table",
+    icon: tableLogo1
+  },
+  {
+    name: "Chair",
+    icon: chairLogo
+  },
+  {
+    name: "Booth Table",
+    icon: boothTableLogo
+  },
+  {
+    name: "Divider",
+    icon: dividerLogo
+  },
+  {
+    name: "Cashier",
+    icon: cashierLogo
+  },
+  {
+    name: "Door",
+    icon: doorLogo
+  }
+]
 
 interface Home {
   update: () => void
@@ -38,7 +80,7 @@ export default function Home({ update, layoutInfo }: Home) {
             onClick={update}>
             Update
           </button>
-          <div id="layout-legend">
+          {/* <div id="layout-legend">
             <ul id="layout-legend-elements">
               <li>
                 <span className="legend-colors">
@@ -61,7 +103,30 @@ export default function Home({ update, layoutInfo }: Home) {
                 Attention
               </li>
             </ul>
-          </div>
+          </div> */}
+          <div className="layout-editor-content">
+          {(GetLayout) && GetLayout.map((data: LayoutDataCell) => {
+            console.log(data.type)
+
+            return (
+                <div
+                  style={{position: 'absolute', top: data.top, left: data.left}}
+                  id={data.id}
+                >
+                  <div 
+                    className = "logo"
+                  >
+                    <img
+                      src={data.icon}
+                      className="editor-item cursor rotate-north"
+                      draggable="false"
+                      />
+                  </div>
+                </div>
+            )
+            
+          })}
+        </div>
         </div>
         <div id="layout-content">
           <Layout colour={layoutInfo} />
