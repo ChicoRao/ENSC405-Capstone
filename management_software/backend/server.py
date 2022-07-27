1b4291fefe3e2eb4a29c9ea20b45a9290b16679c
Yoel Yonata <yoel.yonata@gmail.com>
2022-07-27 12:01:47 -0700

    fixing merge conflict

--- management_software/backend/server.py
+++ management_software/backend/server.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template, request
-# from bowlStatusDetection import bowlStatus
-# from plateStatusDetection import plateStatus
-# from waterRefillDetection import run1
-# from bowlStatusDetection import run2
-# from plateStatusDetection import run3
 from flask import Flask, jsonify, render_template
+<<<<<<< HEAD
 # from waterLevelDetectionBlob import run1
 # from dirtyPlateDetection import run2
+=======
+from ipDetection import ipSearch
+>>>>>>> 143d286 (fixed polling and multiple emit on server)
 from freeOccupiedDetection import freeOccupied
 from imageComparison import compare
 from colours import colours
 from decision import decision
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 from random import random
 from time import sleep
 from flask_cors import CORS
@@ -34,31 +33,28 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 cors = CORS(app)
 
 # app.register_blueprint(water)
-values = {
-    'slider1': 25,
-    'slider2': 0,
-}
 
 @app.route("/capture")
 def capture_photo():
-    img_resp=urllib.request.urlopen(url)
-    imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-    img = cv2.imdecode(imgnp,-1)
-    img_name = "base_photo_.png"
-    cv2.imwrite(img_name, img)
-    # print("{} written!".format(img_name))
-    return img
-
-@app.route("/")
-def index():
-    return render_template('index.html', **values)
+    url1 = urlList[0]
+    url2 = urlList[1]
+    img_resp1=urllib.request.urlopen(url1)
+    img_resp2=urllib.request.urlopen(url2)
+    imgnp1=np.array(bytearray(img_resp1.read()),dtype=np.uint8)
+    imgnp2=np.array(bytearray(img_resp2.read()),dtype=np.uint8)
+    img1 = cv2.imdecode(imgnp1,-1)
+    img2 = cv2.imdecode(imgnp2,-1)
+    img_name1 = "base_photo_1.png"
+    img_name2 = "base_photo_2.png"
+    cv2.imwrite(img_name1, img1)
+    cv2.imwrite(img_name2, img2)
 
 @socketio.on('connect')
 def test_connect():
     emit('after connect',  {'data':'Connected'})
 
-@socketio.on('Slider value changed')
-def value_changed(message, ):
+@socketio.on('start stream')
+def value_changed(message):
     t0 = time.time()
     t1 = time.time()
     # waterqueue = []
@@ -75,16 +71,21 @@ def value_changed(message, ):
         {'status': "Need Cleaning" , 'colour': "red"}
     ]
     while True:
-        # if (time.time() > t1+5):
-        #     i = i%3
-        #     emit('update value', tempTest[i], broadcast=True)
-        #     t1 = time.time()
-        #     i += 1
-        img_resp=urllib.request.urlopen(url)
-        imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
-        img = cv2.imdecode(imgnp,-1)
-        # print(img)
-        if not img.all():
+        img_resp1=urllib.request.urlopen(url1)
+        imgnp1=np.array(bytearray(img_resp1.read()),dtype=np.uint8)
+        img1 = cv2.imdecode(imgnp1,-1)
+        img_resp2=urllib.request.urlopen(url2)
+        imgnp2=np.array(bytearray(img_resp2.read()),dtype=np.uint8)
+        img2 = cv2.imdecode(imgnp2,-1)
+        if not img1.all() and not img2.all():
+            occupancy1 = freeOccupied(img1)
+            occupancy2 = freeOccupied(img2)
+            occupancyqueue1.append(occupancy1)
+            occupancyqueue2.append(occupancy2)
+            comparison1 = compare(img1, "base_photo_1.png")
+            comparison2 = compare(img2, "base_photo_2.png")
+            comparisonqueue1.append(comparison1) 
+            comparisonqueue2.append(comparison2)
             
             # water_level = run1(img)
             # waterqueue.append(water_level) 
@@ -123,22 +124,26 @@ def value_changed(message, ):
                 # bowlqueue.clear()
                 # platequeue.clear()
                 t0 = time.time()
-                if len(decisionqueue) == 2:
-                    print(decisionqueue)
-                    decision_status = decision(decisionqueue)
-
-                    print(decision_status)
-                    objectcolours = colours(decision_status, tableID)
-                    print(objectcolours)
-                    emit('update value', objectcolours, broadcast=True)
-                    decisionqueue.clear()
+                if len(decisionqueue1) == 2 or len(decisionqueue2) == 2:
+                    print("camera1 ", decisionqueue1)
+                    print("camera2 ", decisionqueue2)
+                    decision_status1 = decision(decisionqueue1)
+                    decision_status2 = decision(decisionqueue2)
+                    objectcolours1 = colours(decision_status1, tableID1)
+                    objectcolours2 = colours(decision_status2, tableID2)
+                    sendingDict[tableID1] = objectcolours1
+                    sendingDict[tableID2] = objectcolours2
+                    emit('update value', sendingDict)
+                    decisionqueue1.clear()
+                    decisionqueue2.clear()
+                    sendingDict.clear()
 
 
 
 def randomString():
     #infinite loop of magical random numbers
     number = round(random()*10, 3)
-    return str(number)
+    return number
 
 
 @app.route("/status")
@@ -155,7 +160,6 @@ def message():
 def SaveLayout():
     global SavedLayout 
     SavedLayout = request.data
-    # print(SavedLayout)
     print("recieved")
     return{"message": "Received Layout successfully"}
 
@@ -173,10 +177,7 @@ def SavePassword():
 
 @app.route("/GetLayout", methods = ['GET'])
 def GetLayout():
-    # print(SavedLayout)
-    # print("sending")
     return SavedLayout
-    # return{"message": "Received Layout successfully"}
 
 
 if __name__ == "__main__":
