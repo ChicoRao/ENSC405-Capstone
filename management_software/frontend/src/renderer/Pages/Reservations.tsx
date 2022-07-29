91a7c170ebe05cf845a8d9b719d6d90e228e59e7
Patrick <congchenri.p@gmail.com>
2022-07-29 13:25:10 -0700

    UI update

--- management_software/frontend/src/renderer/Pages/Reservations.tsx
+++ management_software/frontend/src/renderer/Pages/Reservations.tsx
@@ -8,13 +8,15 @@ function App() {
   const maxDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 25);
   return (
   <div>
-    //To check calendar views paste start="Decade" and depth="Year" in below code. Also remove range restriction i.e. min and max properties
-    <CalendarComponent value={dateValue} 
-    min={minDate} 
-    max={maxDate}
-    isMultiSelection={true}
-    start="Decade"
-    depth="Year"></CalendarComponent>
+    <div id="layout-test">
+      <CalendarComponent value={dateValue} 
+      min={minDate} 
+      max={maxDate}
+      //isMultiSelection={true}
+      // start="Decade"
+      // depth="Year">
+      ></CalendarComponent>
+    </div>
   </div>
   );
 }
