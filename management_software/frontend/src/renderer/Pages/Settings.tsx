f66f171fcf05699b4c6d5ae8770732a53fd80036
angus kan <32191361+KMBD@users.noreply.github.com>
2022-08-04 21:49:06 -0700

    got rid of devtools

--- management_software/frontend/src/renderer/Pages/Settings.tsx
+++ management_software/frontend/src/renderer/Pages/Settings.tsx
@@ -10,29 +10,29 @@ export default function Settings() {
     }
 
 	return(
-        // <div className='WifiInput'>
-        //     <div>
-        //         <form>
-        //             <label>
-        //                 Wifi Name:
-        //                 <input id="wifiName" type="text" name="WifiName" />
-        //             </label>
-        //         </form>
-        //     </div>
-        //     <div>
-        //         <form>
-        //             <label>
-        //                 Password:
-        //                 <input id="wifiPassword" type="text" name="Password" />
-        //             </label>
-        //         </form>
-        //     </div>
-        //     <div className="saveWifiButtonDiv">
-        //         <button className="saveWifiButton" onClick={save}> Save </button>
-        //     </div>
-            <div className="updatebutton">
-                <Button type="primary"> Update </Button>
+        <div className='WifiInput'>
+            <div>
+                <form>
+                    <label>
+                        Wifi Name:
+                        <input id="wifiName" type="text" name="WifiName" />
+                    </label>
+                </form>
             </div>
-        // </div>
+            <div>
+                <form>
+                    <label>
+                        Password:
+                        <input id="wifiPassword" type="text" name="Password" />
+                    </label>
+                </form>
+            </div>
+            <div className="saveWifiButtonDiv">
+                <button className="saveWifiButton" onClick={save}> Save </button>
+            </div>
+            {/* <div className="updatebutton">
+                <Button type="primary"> Update </Button>
+            </div> */}
+        </div>
     );
 };
