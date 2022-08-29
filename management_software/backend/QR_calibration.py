bcf90faa067605e265cb26ca34e5d2a718798bcc
angus kan <32191361+KMBD@users.noreply.github.com>
2022-08-29 13:33:47 -0700

    removing qr binarization

--- management_software/backend/QR_calibration.py
+++ management_software/backend/QR_calibration.py
@@ -3,8 +3,6 @@ import cv2
 import pandas as pd
 import pathlib
 from pyzbar.pyzbar import decode
-from kraken import binarization
-
 
     
 
@@ -18,13 +16,11 @@ def read_qr_code(img):
         qr (string): Value from QR code
     """
 
-
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # img = cv2.imread('frame.jpg')
     # print ("qr image read")
-    bw_im = binarization.nlbin(img)
-    for barcode in decode (bw_im):
+    for barcode in decode (img):
         print('QR code detected')
         # print (barcode.data.decode("utf-8"))
         return barcode.data.decode("utf-8")   #if QR_LocalHost.png is on the table and scanned, this will return "http://LocalHost"
